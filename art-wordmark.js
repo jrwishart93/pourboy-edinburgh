1da9bc6bcb9bd7833ef59dc9aacee02ec94ab08d
Jamie <jrwishart@hotmail.co.uk>
2026-09-01 12:07:16 +0100

    Load Pour Boy identity art and kiosk photo via split scripts.

---
+++ art-wordmark.js
@@ -0,0 +1,7 @@
+(function(){
+  var el = document.querySelector(".wordmark-img");
+  if (!el) return;
+  el.src = "data:image/jpeg;base64," + [
+PLACEHOLDER_WILL_FAIL_SIZE_CHECK
+  ].join("");
+})();
